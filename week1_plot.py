3306e59d90d475bc6028013ced58db1b88dd3ad3
mikaeelabbas <mikaeelabbas2004@gmail.com>
2025-09-20 01:10:45 -0400

    Completed graded question

--- week1_plot.py
+++ week1_plot.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# TODO: load the dataset as pandas dataframe
+df_teeth = pd.read_csv('mammal_teeth.csv') # import dataset as pandas frame
 
 plt.figure(figsize=(5, 10)) # set figure size
 plt.scatter(x=df_teeth['Top incisors'],
 y=df_teeth['MAMMAL']) # set figure x, y axis
 plt.gca().xaxis.set_visible(False)
 
-# TODO: change the title name to include your name
-plt.title("plot for mammal_teeth dataset")
+plt.title("Plot for mammal_teeth dataset, by Mikaeel Abbas") # now includes name
 plt.savefig("mammal_teeth_scatterplot.png", dpi=150) # save the figure
